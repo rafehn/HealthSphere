28c4d0545729bb9b68cdcc1eb47e16fc80ec33f0
rafeyN <rafehnawaz@gmail.com>
2023-05-18 17:48:26 +0100

    commit 2

--- App.js
+++ App.js
@@ -1,14 +1,131 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { StatusBar } from 'react-native';
-import store from './redux/store/store';
-import AppNavigator from './navigation/AppNavigator';
+// App.js
+import React, { useState } from "react";
+import DietPlans from "./components/DietPlans";
+import HealthTracker from "./components/HealthTracker";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const [data, setData] = useState("");
+
+  const fetchData = async () => {
+    const response = await fetch("https://api.github.com/users/bard");
+    const json = await response.json();
+    setData(json.name);
+  };
+
+  const saveData = async () => {
+    await AsyncStorage.setItem("name", data);
+  };
+
+  const loadData = async () => {
+    const name = await AsyncStorage.getItem("name");
+    setData(name);
+  };
+
+  const [dietPlans, setDietPlans] = useState([]);
+
+  const addDietPlan = () => {
+    const newDietPlan = {
+      name: "",
+      calories: 0,
+      fat: 0,
+      carbohydrates: 0,
+      protein: 0,
+    };
+
+    setDietPlans([...dietPlans, newDietPlan]);
+  };
+
+  const removeDietPlan = (index) => {
+    setDietPlans(dietPlans.filter((dietPlan, i) => i !== index));
+  };
+
   return (
-    <Provider store={store}>
-      <StatusBar barStyle="light-content" />
-      <AppNavigator />
-    </Provider>
+    <View style={styles.container}>
+      <Text>Hello, world!</Text>
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={text => setData(text)}
+      />
+      <Button
+        title="Fetch Data"
+        onPress={fetchData}
+      />
+      <Button
+        title="Save Data"
+        onPress={saveData}
+      />
+      <Button
+        title="Load Data"
+        onPress={loadData}
+      />
+      <Text>{data}</Text>
+      <HealthTracker />
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "black",
+    margin: 10,
+  },
+  list: {
+    margin: 10,
+  },
+  item: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: "lightgray",
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+  },
+  calories: {
+    fontSize: 14,
+    color: "black",
+  },
+  fat: {
+    fontSize: 14,
+    color: "black",
+  },
+  carbohydrates: {
+    fontSize: 14,
+    color: "black",
+  },
+  protein: {
+    fontSize: 14,
+    color: "black",
+  },
+  removeButton: {
+    backgroundColor: "red",
+    color: "white",
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+  },
+  addButton: {
+    backgroundColor: "green",
+    color: "white",
+    borderRadius: 5,
+    padding: 10,
+    margin: 10,
+  },
+});
+
+export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,77 +1,62 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { connect } from 'react-redux';
-import NutritionInfo from '../components/NutritionInfo';
-import Button from '../components/Button';
+// App.js
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+} from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function HomeScreen({navigation}){
+const App = () => {
+  const [data, setData] = useState("");
+
+  const fetchData = async () => {
+    const response = await fetch("https://api.github.com/users/bard");
+    const json = await response.json();
+    setData(json.name);
+  };
+
+  const saveData = async () => {
+    await AsyncStorage.setItem("name", data);
+  };
+
+  const loadData = async () => {
+    const name = await AsyncStorage.getItem("name");
+    setData(name);
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>HealthSphere</Text>
-      <View style={styles.nutritionInfoContainer}>
-        <NutritionInfo
-          // label="Calories Burnt"
-          title="Calories Burnt"
-          value={23.2}
-          // value={caloriesBurnt}
-          unit="kcal"
-          color="#f4511e"
-        />
-        <NutritionInfo
-          title="Steps Walked"
-          value={32.2}
-          unit="steps"
-          color="#8BC34A"
-        />
-        <NutritionInfo
-          title="Diet Plan"
-          value={23.4}
-          unit=""
-          color="#2196F3"
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <Button
-          label="View Calories"
-          onPress={() => console.log('View Calories Pressed')}
-          color="#f4511e"
-        />
-        <Button
-          label="View Diet Plan"
-          onPress={() => console.log('View Diet Plan Pressed')}
-          color="#2196F3"
-        />
-      </View>
+      <Text>Hello, world!</Text>
+      <TextInput
+        placeholder="Enter your name"
+        onChangeText={text => setData(text)}
+      />
+      <Button
+        title="Fetch Data"
+        onPress={fetchData}
+      />
+      <Button
+        title="Save Data"
+        onPress={saveData}
+      />
+      <Button
+        title="Load Data"
+        onPress={loadData}
+      />
+      <Text>{data}</Text>
     </View>
   );
 };
 
-const mapStateToProps = state => ({
-  caloriesBurnt: state.caloriesBurnt,
-  stepsWalked: state.stepsWalked,
-  dietPlan: state.dietPlan,
-});
-
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
-    paddingHorizontal: 20,
-    paddingTop: 50,
-  },
-  heading: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  nutritionInfoContainer: {
-    // flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 40,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
+
+export default App;
